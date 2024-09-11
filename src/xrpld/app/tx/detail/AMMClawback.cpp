4881061a22a4479774d1373a3e02d43f78708dba
yinyiqian1 <yqian@ripple.com>
2024-09-11 11:27:19 -0400

    Add AMMClawback transaction
    Add AMMClawback transaction
    
    Add AMMClawback transaction
    
    Add AMMClawback transaction

--- src/xrpld/app/tx/detail/AMMClawback.cpp
+++ src/xrpld/app/tx/detail/AMMClawback.cpp
@@ -17,7 +17,6 @@
 */
 //==============================================================================
 
-// #include <xrpl/protocol/AmountConversions.h>
 #include <xrpld/app/misc/AMMHelpers.h>
 #include <xrpld/app/misc/AMMUtils.h>
 #include <xrpld/app/tx/detail/AMMClawback.h>
@@ -43,54 +42,40 @@ AMMClawback::preflight(PreflightContext const& ctx)
     if (auto const ret = preflight1(ctx); !isTesSuccess(ret))
         return ret;
 
-    if (ctx.tx.getFlags() & tfClawbackMask)
+    if (ctx.tx.getFlags() & tfAMMClawbackMask)
         return temINVALID_FLAG;
 
     AccountID const issuer = ctx.tx[sfAccount];
     AccountID const holder = ctx.tx[sfHolder];
-    AccountID const ammAccount = ctx.tx[sfAMMAccount];
-    
+
     if (issuer == holder)
     {
-        JLOG(ctx.j.debug())
+        JLOG(ctx.j.trace())
             << "AMMClawback: holder cannot be the same as issuer.";
-        std::cout << "AMMClawback: holder cannot be the same as issuer."
-                  << std::endl;
         return temMALFORMED;
     }
 
     std::optional<STAmount> const clawAmount = ctx.tx[~sfAmount];
+    auto const asset = ctx.tx[sfAsset];
 
-    std::cout << "preflight: issuer " << issuer << std::endl;
-    std::cout << "preflight: holder " << holder << std::endl;
-    std::cout << "preflight: ammAccount " << ammAccount << std::endl;
-    if (clawAmount)
+    if (asset.account != issuer)
     {
-        std::cout << "clawAmout issuer: " << clawAmount->getIssuer()
-                  << std::endl;
-        std::cout << "clawAmout mantissa: " << clawAmount->mantissa()
-                  << std::endl;
-        std::cout << "clawAmout exponent: " << clawAmount->exponent()
-                  << std::endl;
+        JLOG(ctx.j.trace()) << "AMMClawback: Amount's issuer subfield should "
+                               "be the same as Account field.";
+        return temBAD_ASSET_ISSUER;
     }
 
-    if (clawAmount && clawAmount->getIssuer() != issuer)
+    if (clawAmount && clawAmount->issue() != asset)
     {
-        JLOG(ctx.j.debug()) << "AMMClawback: Amount's issuer subfield should "
-                               "be the same as Account field.";
-        std::cout << "AMMClawback: Amount's issuer subfield should be the same "
-                     "as Account field."
-                  << std::endl;
-        return temBAD_AMOUNT;
+        JLOG(ctx.j.trace()) << "AMMClawback: Amount's issuer/currency subfield "
+                               "does not match Asset field";
+        return temBAD_ASSET_AMOUNT;
     }
 
     if (*clawAmount < beast::zero)
     {
-        JLOG(ctx.j.debug())
+        JLOG(ctx.j.trace())
             << "AMMClawback: Amount being clawed back can't be less than zero.";
-        std::cout
-            << "AMMClawback: Amount being clawed back can't be less than zero."
-            << std::endl;
         return temBAD_AMOUNT;
     }
 
@@ -112,39 +97,53 @@ AMMClawback::preclaim(PreclaimContext const& ctx)
     {
         JLOG(ctx.j.debug())
             << "AMMClawback: AMMAccount provided does not exist.";
-        return temMALFORMED;
+        return terNO_AMM;
     }
+
+    std::uint32_t const issuerFlagsIn = sleIssuer->getFieldU32(sfFlags);
+
+    // If AllowTrustLineClawback is not set or NoFreeze is set, return no
+    // permission
+    if (!(issuerFlagsIn & lsfAllowTrustLineClawback) ||
+        (issuerFlagsIn & lsfNoFreeze))
+        return tecNO_PERMISSION;
 
     auto const ammID = sleAMMAccount->getFieldH256(sfAMMID);
     if (!ammID)
     {
-        JLOG(ctx.j.debug()) << "AMMClawback: AMMAccount field is not an AMM account.";
-        return tecNO_PERMISSION;
+        JLOG(ctx.j.trace())
+            << "AMMClawback: AMMAccount field is not an AMM account.";
+        return tecINTERNAL;
     }
-
-    std::cout << "amm ID: " << ammID << std::endl;
 
     auto const sleAMM = ctx.view.read(keylet::amm(ammID));
     if (!sleAMM)
     {
-        JLOG(ctx.j.debug()) << "AMMClawback: can not find AMM with ammID: " << ammID;
+        JLOG(ctx.j.trace())
+            << "AMMClawback: can not find AMM with ammID: " << ammID;
         return tecINTERNAL;
     }
 
     STIssue const& asset = sleAMM->getFieldIssue(sfAsset);
     STIssue const& asset2 = sleAMM->getFieldIssue(sfAsset2);
 
-    std::cout << "asset currency: " << asset.issue().currency << std::endl;
-    std::cout << "asset2 currency: " << asset2.issue().currency << std::endl;
-
-    if (asset.issue().account != issuer && asset2.issue().account != issuer)
+    if (ctx.tx[sfAsset] != asset && ctx.tx[sfAsset] != asset2)
     {
-        JLOG(ctx.j.debug())
-            << "AMMClawback: Account field is not the issuer of either asset "
-               "in AMM pool. The two assets in the pool are: "
-            << asset.issue().currency << " and " << asset2.issue().currency;
-
+        JLOG(ctx.j.trace()) << "AMMClawback: Asset being clawed back does not "
+                               "match either asset in the AMM pool.";
         return tecNO_PERMISSION;
+    }
+
+    auto const flags = ctx.tx.getFlags();
+    if (flags & tfClawTwoAssets)
+    {
+        if (asset.issue().account != asset2.issue().account)
+        {
+            JLOG(ctx.j.trace())
+                << "AMMClawback: tfClawTwoAssets can only be enabled when two "
+                   "assets in the AMM pool are both issued by the issuer";
+            return tecNO_PERMISSION;
+        }
     }
 
     return tesSUCCESS;
@@ -169,27 +168,27 @@ AMMClawback::applyGuts(Sandbox& sb)
     AccountID const ammAccount = ctx_.tx[sfAMMAccount];
     AccountID const issuer = ctx_.tx[sfAccount];
     AccountID const holder = ctx_.tx[sfHolder];
+    Issue const asset = ctx_.tx[sfAsset];
 
     auto const sleAMMAccount = ctx_.view().read(keylet::account(ammAccount));
-
     if (!sleAMMAccount)
     {
-        JLOG(j_.debug()) << "AMMClawback: AMMAccount provided does not exist.";
-        return {temMALFORMED, false};
+        JLOG(j_.trace()) << "AMMClawback: AMMAccount provided does not exist.";
+        return {terNO_AMM, false};
     }
 
     auto const ammID = sleAMMAccount->getFieldH256(sfAMMID);
     if (!ammID)
     {
-        JLOG(j_.debug())
+        JLOG(j_.trace())
             << "AMMClawback: AMMAccount field is not an AMM account.";
-        return {tecNO_PERMISSION, false};
+        return {tecINTERNAL, false};
     }
 
     auto ammSle = sb.peek(keylet::amm(ammID));
     if (!ammSle)
     {
-        JLOG(j_.debug()) << "AMMClawback: can not find AMM with ammID: "
+        JLOG(j_.trace()) << "AMMClawback: can not find AMM with ammID: "
                          << ammID;
         return {tecINTERNAL, false};
     }
@@ -198,23 +197,14 @@ AMMClawback::applyGuts(Sandbox& sb)
     Issue const& issue1 = ammSle->getFieldIssue(sfAsset).issue();
     Issue const& issue2 = ammSle->getFieldIssue(sfAsset2).issue();
 
-    if (!clawAmount)
-        return clawbackAll(sb, ammSle, ammAccount, issuer, holder);
-
-    Issue currentIssue = clawAmount->issue();
-    Issue otherIssue;
-
-    if (currentIssue == issue1)
+    Issue otherIssue = issue1;
+    if (asset == issue1)
         otherIssue = issue2;
-    else if (currentIssue == issue2)
-        otherIssue = issue1;
-    else
-        return {tecINTERNAL, false};
 
     auto const expected = ammHolds(
         sb,
         *ammSle,
-        currentIssue,
+        asset,
         otherIssue,
         FreezeHandling::fhZERO_IF_FROZEN,
         ctx_.journal);
@@ -223,97 +213,41 @@ AMMClawback::applyGuts(Sandbox& sb)
         return {expected.error(), false};
     auto const [amountBalance, amount2Balance, lptAMMBalance] = *expected;
 
-    auto [result, newLPTokenBalance] = equalWithdrawMatchingOneAmount(
-        sb,
-        *ammSle,
-        holder,
-        ammAccount,
-        amountBalance,
-        amount2Balance,
-        lptAMMBalance,
-        *clawAmount,
-        tfee);
+    TER result;
+    STAmount newLPTokenBalance;
+    STAmount amountWithdraw;
+    std::optional<STAmount> amount2Withdraw;
 
-    if (result != tesSUCCESS)
-        return {result, false};
-
-    auto const res = deleteAMMAccountIfEmpty(
-        sb, ammSle, newLPTokenBalance, issue1, issue2, j_);
-    if (!res.second)
-        return {res.first, false};
-
-    JLOG(ctx_.journal.trace())
-        << "AMM Withdraw during AMMClawback: lptoken new balance: "
-        << to_string(newLPTokenBalance.iou())
-        << " old balance: " << to_string(lptAMMBalance.iou());
-
-    auto const ter = rippleCredit(sb, holder, issuer, *clawAmount, true, j_);
-
-    if (ter != tesSUCCESS)
-        return {ter, false};
-
-    return {tesSUCCESS, true};
-}
-
-std::pair<TER, bool>
-AMMClawback::clawbackAll(
-    Sandbox& sb,
-    const std::shared_ptr<SLE> ammSle,
-    AccountID const& ammAccount,
-    AccountID const& issuer,
-    AccountID const& holder)
-{
-    auto const tfee = getTradingFee(ctx_.view(), *ammSle, ammAccount);
-    Issue const& issue1 = ammSle->getFieldIssue(sfAsset).issue();
-    Issue const& issue2 = ammSle->getFieldIssue(sfAsset2).issue();
-    bool accountIssuesBoth = false;
-    Issue currentIssue = issue1;
-    Issue otherIssue = issue2;
-    if (issue1.account == issuer && issue2.account == issuer)
-        accountIssuesBoth = true;
-    else if (issue1.account != issuer && issue2.account == issuer)
+    if (!clawAmount)
     {
-        currentIssue = issue2;
-        otherIssue = issue1;
+        auto const holdLPtokens = ammLPHolds(sb, *ammSle, holder, j_);
+        std::tie(result, newLPTokenBalance, amountWithdraw, amount2Withdraw) =
+            AMMWithdraw::equalWithdrawTokens(
+                sb,
+                *ammSle,
+                holder,
+                ammAccount,
+                amountBalance,
+                amount2Balance,
+                lptAMMBalance,
+                holdLPtokens,
+                holdLPtokens,
+                tfee,
+                ctx_.journal,
+                ctx_.tx);
     }
-    else if (issue1.account != issuer && issue2.account != issuer)
-        return {tecINTERNAL, false};
-
-    auto const expected = ammHolds(
-        sb,
-        *ammSle,
-        currentIssue,
-        otherIssue,
-        FreezeHandling::fhZERO_IF_FROZEN,
-        ctx_.journal);
-
-    if (!expected)
-        return {expected.error(), false};
-    auto const [amountBalance, amount2Balance, lptAMMBalance] = *expected;
-
-    auto const lpTokenBalance = ammSle->getFieldAmount(sfLPTokenBalance);
-    STAmount const holdLPtokens = accountHolds(
-        view(),
-        holder,
-        lpTokenBalance.getCurrency(),
-        lpTokenBalance.getIssuer(),
-        fhIGNORE_FREEZE,
-        j_);
-
-    auto [result, newLPTokenBalance, amount1Withdraw, amount2Withdraw] =
-        AMMWithdraw::equalWithdrawTokens(
-            sb,
-            *ammSle,
-            holder,
-            ammAccount,
-            amountBalance,
-            amount2Balance,
-            lptAMMBalance,
-            holdLPtokens,
-            holdLPtokens,
-            tfee,
-            ctx_.journal,
-            ctx_.tx);
+    else
+        std::tie(result, newLPTokenBalance, amountWithdraw, amount2Withdraw) =
+            equalWithdrawMatchingOneAmount(
+                sb,
+                *ammSle,
+                holder,
+                ammAccount,
+                amountBalance,
+                amount2Balance,
+                lptAMMBalance,
+                *clawAmount,
+                tfee);
 
     if (result != tesSUCCESS)
         return {result, false};
@@ -328,17 +262,19 @@ AMMClawback::clawbackAll(
         << to_string(newLPTokenBalance.iou())
         << " old balance: " << to_string(lptAMMBalance.iou());
 
-    auto const ter =
-        rippleCredit(sb, holder, issuer, amount1Withdraw, true, j_);
-
+    auto const ter = rippleCredit(sb, holder, issuer, amountWithdraw, true, j_);
     if (ter != tesSUCCESS)
         return {ter, false};
 
-    if (accountIssuesBoth)
+    auto const flags = ctx_.tx.getFlags();
+    if (flags & tfClawTwoAssets)
     {
+        // if the issuer issues both assets and sets flag tfClawTwoAssets, we
+        // will claw the paired asset as well. We already checked if
+        // tfClawTwoAssets is enabled, the two assets have to be issued by the
+        // same issuer.
         auto const ter =
             rippleCredit(sb, holder, issuer, *amount2Withdraw, true, j_);
-
         if (ter != tesSUCCESS)
             return {ter, false};
     }
@@ -346,9 +282,9 @@ AMMClawback::clawbackAll(
     return {tesSUCCESS, true};
 }
 
-std::tuple<TER, STAmount>
+std::tuple<TER, STAmount, STAmount, std::optional<STAmount>>
 AMMClawback::equalWithdrawMatchingOneAmount(
-    Sandbox& view,
+    Sandbox& sb,
     SLE const& ammSle,
     AccountID const& holder,
     AccountID const& ammAccount,
@@ -360,10 +296,29 @@ AMMClawback::equalWithdrawMatchingOneAmount(
 {
     auto frac = Number{amount} / amountBalance;
     auto const amount2Withdraw = amount2Balance * frac;
-    TER result;
-    STAmount newLPTokenBalance;
-    std::tie(result, newLPTokenBalance, std::ignore, std::ignore) = withdraw(
-        view,
+
+    auto const lpTokensWithdraw =
+        toSTAmount(lptAMMBalance.issue(), lptAMMBalance * frac);
+    auto const holdLPtokens = ammLPHolds(sb, ammSle, holder, j_);
+    if (lpTokensWithdraw > holdLPtokens)
+        // if lptoken balance less than what the issuer intended to clawback,
+        // clawback all the tokens
+        return AMMWithdraw::equalWithdrawTokens(
+            sb,
+            ammSle,
+            holder,
+            ammAccount,
+            amountBalance,
+            amount2Balance,
+            lptAMMBalance,
+            holdLPtokens,
+            holdLPtokens,
+            tfee,
+            ctx_.journal,
+            ctx_.tx);
+
+    return withdraw(
+        sb,
         ammAccount,
         holder,
         ammSle,
@@ -375,7 +330,6 @@ AMMClawback::equalWithdrawMatchingOneAmount(
         tfee,
         ctx_.journal,
         ctx_.tx);
-    return {result, newLPTokenBalance};
 }
 
 }  // namespace ripple
